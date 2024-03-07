df310ea811aa2946b3b7115def94ca24359e1799
selcancamsari <61784573+selcancamsari@users.noreply.github.com>
2024-03-07 15:54:24 +0300

    message acknowledgement, BasicQos, BasicAck

--- RabbitMqConsumer/Program.cs
+++ RabbitMqConsumer/Program.cs
@@ -16,16 +16,28 @@ using IModel channel = connection.CreateModel();
 //exclusive parametresi kuyruğun özel olup olmadığı. Birden fazla kuyruk ile özel olarak işlem yapıp yapamayacağımızı belirtir.
 //autoDelete tüm mesajların tüketildiğinde kuyruğun silinip silinemeyeceğine dair yapılanmadır.
 //consumer'da da kuyruk publisherdaki ile birebir aynı yapılanmada tanımlanmalıdır.
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
 
 //Queue'dan Mesaj Okuma
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue:"example-queue", autoAck:false, consumer);
+//autoAck: false ise mesaj onaylama süreci aktifleştirmiş oluruz.
+//message acknowledgement
+channel.BasicConsume(queue:"example-queue", autoAck:false, consumer: consumer);
+//prefetchSize: mesaj boyutu byte cinsinden. 0 sınırsız demek
+//preFetchCount: aynı anda işleme alınabilecek mesaj sayısı
+//global: tüm consumerlar için mi yoksa belirli bir consumer için mi
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1,global: false);
 consumer.Received += (sender , e) =>
 {
     //Kuyruğa gelen mesajın işlendiği yer.
     //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getiriir.
     //e.Body.Span veya e.Body.ToArray fonksiyonları kuyruktaki mesajın byte verisini getirecektir.
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    //yalnızca bu mesajı doğrulamak için
+    //multiple: false 
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+
+
 };
 Console.Read();
